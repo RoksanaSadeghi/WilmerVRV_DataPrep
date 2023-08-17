6f9cb80c49ada086dc8f1de9fab9528363974874
Roksana Sadeghi <roksana.s.sadeghi@gmail.com>
2023-08-17 12:53:06 -0700

    version

--- WilmerVRV_DataPrep/WilmerVRV_DataPrep/obj/Release/net7.0-windows/AssemblyInfo.g.cs
+++ WilmerVRV_DataPrep/WilmerVRV_DataPrep/obj/Release/net7.0-windows/AssemblyInfo.g.cs
@@ -8,6 +8,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0+Branch.main.Sha.0e8e89240fe0f73b99d724eef093db79d35bb0bf")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1+3.Branch.main.Sha.9ed3ad6c0fccd34ef941c4bb8803f50f41544c8e")]
